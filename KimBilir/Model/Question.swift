6a6527b199846ceffe82cd6acf5262b8dd1ed60e
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-10-16 12:21:17 +0300

    Creating Question model

---
+++ KimBilir/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  KimBilir
+//
+//  Created by Sevde Aydın on 16.10.2024.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let answers: [String]
+    let correctAnswer: String
+}
---
+++ KimBilir/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  KimBilir
+//
+//  Created by Sevde Aydın on 16.10.2024.
+//
+
+import Foundation
